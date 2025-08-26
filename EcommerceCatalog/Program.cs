52e4811b01bae2645797b35dd9ee1d8f071b445a
chris <christophermaina003@gmail.com>
2025-08-26 22:09:50 +0300

    Seed Default Categories on start of app

--- EcommerceCatalog/Program.cs
+++ EcommerceCatalog/Program.cs
@@ -1,5 +1,6 @@
 using EcommerceCatalog.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,6 +20,27 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+// Database seeding
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+
+    // Ensure DB exists & apply migrations
+    context.Database.Migrate();
+
+    // Seed categories if none exist
+    if (!context.Categories.Any())
+    {
+        context.Categories.AddRange(
+            new Category { Name = "Electronics" },
+            new Category { Name = "Clothing" },
+            new Category { Name = "Books" }
+        );
+        context.SaveChanges();
+    }
 }
 
 app.UseHttpsRedirection();
