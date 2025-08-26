14a77ed8734055905115f50cdf028caa0b046fad
chris <christophermaina003@gmail.com>
2025-08-26 12:52:19 +0300

    Set Up the DB connection string

--- EcommerceCatalog/Program.cs
+++ EcommerceCatalog/Program.cs
@@ -1,7 +1,15 @@
+using EcommerceCatalog.Models;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add EF Core + SQL Server connection
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
